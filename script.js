045ca883bfd6a205618993a3e56d886cfa0ac853
bea3araujo <beatrizaraujo2507@gmail.com>
2021-05-16 19:03:22 -0300

    criação de 4 loops, 3 for e 1 while. Desafio VnW

---
+++ script.js
@@ -0,0 +1,21 @@
+//1//
+for( let ex1 = 1; ex1 <= 10; ex1++){
+    console.log(ex1)
+}
+
+//2//
+let ex2 = 10
+while(ex2 >= 1){
+    console.log(ex2)
+    ex2--
+}
+
+//3//
+for( let ex3 = 1; ex3 <= 100; ex3 += 2){
+    console.log(ex3)
+}
+
+//4//
+for(let ex4 = 0; ex4 <= 100; ex4 += 2){
+    console.lg(ex4)
+}
